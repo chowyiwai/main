5bdd6b59b37398421f3bf3d80b499f5559671082
chowyiwai <yiwai.chow99@gmail.com>
2019-10-22 20:39:11 +0800

    Fix checkstyle issue

--- src/main/java/seedu/address/ui/TagListCard.java
+++ src/main/java/seedu/address/ui/TagListCard.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import seedu.address.model.Color;
 import seedu.address.model.tag.Tag;
 
 /**
