5d2f6f97e0da5f72edd4b03e0154ef74a5150036
chowyiwai <yiwai.chow99@gmail.com>
2019-10-26 16:09:05 +0800

    Modify RenameTagCommand + add test

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.module.Module;
 import seedu.address.model.module.UniqueModuleList;
 import seedu.address.model.tag.Tag;
